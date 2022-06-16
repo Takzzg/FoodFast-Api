cc0a9b6b7da1ff9ad23cb6427e691a98e90afd2a
Guido Queiroz <Takzzg@hotmail.com>
2022-06-16 20:56:13 -0300

    fake login working (?

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,41 +1,56 @@
-import bcryptjs from 'bcryptjs';
+import bcryptjs from "bcryptjs"
 import User from "../models/user.js"
 
-export const postUser = async(req,res) =>{
-  try{
-    const {name,email,password,rol}=req.body;
+export const postUser = async (req, res) => {
+    try {
+        const { name, email, password, rol } = req.body
 
-   
-    const users=new User({name,email,password,rol});
-     
-    //Encriptar password
-    const salt=bcryptjs.genSaltSync() //hacer más complicado el método de encriptación 
-    users.password=bcryptjs.hashSync(password,salt);  
-    await users.save()
+        const users = new User({ name, email, password, rol })
 
-    res.json({users})
-  }catch(e){
-      console.log(e)
-  }
-    
-   
+        //Encriptar password
+        const salt = bcryptjs.genSaltSync() //hacer más complicado el método de encriptación
+        users.password = bcryptjs.hashSync(password, salt)
+        await users.save()
+
+        res.json({ users })
+    } catch (e) {
+        console.log(e)
+    }
 }
 
+export const getAllUsers = async (req, res) => {
+    try {
+        const user = await User.find()
+        if (!user)
+            return res.status(400).json({
+                msg: "No hay usuarios para mostrar"
+            })
 
-export const getUser = async(req,res) =>{
-  try{
-    const user=await User.find()
-    if(!user){
-      return res.status(400).json({
-        msg:"No hay usuarios para mostrar"
-      })
+        let basicInfo = user.map((u) => ({ name: u.name, email: u.email }))
+        res.status(200).json(basicInfo)
+    } catch (e) {
+        console.log(e)
     }
-    return res.status(200).json({
-      user
-    })
-  }catch(e){
-    console.log(e)
-  }
-     
-   
+}
+
+export const logIn = async (req, res) => {
+    try {
+        const { email, password } = req.body
+
+        if (!email || !password) {
+            console.log("missing credentials")
+            return res.status(500).json({ error: "missing credentials" })
+        }
+
+        // FAKE LOGIN
+        const user = await User.find({ email })
+        // const match = user.password === password
+        const match = true
+
+        if (match) return res.json(user[0])
+        return res.status(500).json({ error: "credentials mismatch" })
+        // FAKE LOGIN
+    } catch (error) {
+        console.log(error)
+    }
 }
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,25 +1,31 @@
-import express from 'express';
-import { check } from 'express-validator';
-import { emailExiste } from '../../middlewares/db-validators.js';
-import { validarCampos } from '../../middlewares/validar-campo.js';
+import express from "express"
+import { check } from "express-validator"
+import { emailExiste } from "../../middlewares/db-validators.js"
+import { validarCampos } from "../../middlewares/validar-campo.js"
 
-import  {postUser,getUser} from '../controllers/userControllers.js'
+import { postUser, getAllUsers, logIn } from "../controllers/userControllers.js"
 
 const router = express.Router()
 
-//post  http://localhost:3001/api/v1/user
-router.post('/',[
-    check("name","El name es obligatorio").not().isEmpty(), 
-    check("password","El password debe de ser más de 6 letras").isLength({min:6}),
-    check("email","El email no es válido").isEmail(), 
-    check("email").custom(emailExiste),
-    validarCampos
-], postUser)
+//POST http://localhost:3001/api/v1/user
+router.post(
+    "/",
+    [
+        check("name", "El name es obligatorio").not().isEmpty(),
+        check("password", "El password debe de ser más de 6 letras").isLength({
+            min: 6
+        }),
+        check("email", "El email no es válido").isEmail(),
+        check("email").custom(emailExiste),
+        validarCampos
+    ],
+    postUser
+)
 
-//get  http://localhost:3001/api/v1/user
-router.get('/', getUser)
+//GET  http://localhost:3001/api/v1/user
+router.get("/", getAllUsers)
 
+//POST  http://localhost:3001/api/v1/user/login
+router.post("/login", logIn)
 
-
-
-export default router;
+export default router
