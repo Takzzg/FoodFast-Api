3eadad3413f85f2e5a2c6aa4459767aaee890b06
Guido Queiroz <Takzzg@hotmail.com>
2022-06-29 12:45:45 -0300

    fix localhost in api/

--- src/models/user.js
+++ src/models/user.js
@@ -1,35 +1,34 @@
-import mongoose from 'mongoose'
-import bcrypt from "bcryptjs";
-import crypto from 'crypto'
-import emailer from '../../ultis/email.js'
+import mongoose from "mongoose"
+import bcrypt from "bcryptjs"
+import crypto from "crypto"
+import emailer from "../../ultis/email.js"
 
-import Token from './token.js'
+import Token from "./token.js"
 
-const userSchema=new mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true
+    email: {
+        type: String,
+        required: true,
+        unique: true
     },
-    password:{
-        type:String,
-        required:true
+    password: {
+        type: String,
+        required: true
     },
-    photo:{
-        type:String
+    photo: {
+        type: String
     },
-    rol:{
-        type:String,
-        required:true,
-        default:"USER",
-        enum:["ADMIN", "USER", "GUEST", "OWNER"]
-
+    rol: {
+        type: String,
+        required: true,
+        default: "USER",
+        enum: ["ADMIN", "USER", "GUEST", "OWNER"]
     },
-    isGoogleAccount:{
+    isGoogleAccount: {
         type: Boolean,
         required: true,
         default: false
@@ -38,53 +37,62 @@ const userSchema=new mongoose.Schema({
         type: Array,
         default: []
     },
-   address:{
-       type:String,   
-       default: "EE UU"
-   },
-   passwordReset: String,
-   passwordResetTokenExpire: Date,
-   verifyAccount:{
-        type: Boolean, 
-        default: false,
-   }
-    
-})
-
-userSchema.pre('save', async function(next){
-    const user = this;
-    if(!user.isModified('password')) return next()
-    try{
-        const salt = await bcrypt.genSalt(10)
-        user.password = await bcrypt.hash(user.password, salt)
-        next()
-    }catch(error){
-        console.log(error)
-        throw new Error ("Error bcrypt password")
+    address: {
+        type: String,
+        default: "EE UU"
+    },
+    passwordReset: String,
+    passwordResetTokenExpire: Date,
+    verifyAccount: {
+        type: Boolean,
+        default: false
     }
 })
 
-userSchema.methods.comparePassword = async function(passwordCandidate){
+userSchema.pre("save", async function (next) {
+    const user = this
+    if (!user.isModified("password")) return next()
+    try {
+        const salt = await bcrypt.genSalt(10)
+        user.password = await bcrypt.hash(user.password, salt)
+        next()
+    } catch (error) {
+        console.log(error)
+        throw new Error("Error bcrypt password")
+    }
+})
+
+userSchema.methods.comparePassword = async function (passwordCandidate) {
     return await bcrypt.compare(passwordCandidate, this.password)
 }
 
-userSchema.methods.send_emailWelcome = async function (cb){
-    const tokenVerify = new Token({_userId: this.id, token: crypto.randomBytes(16).toString('hex')});
-    const email_destination = this.email;
-    tokenVerify.save( (err)=>{
-        if(err) { return console.log(err.message)}
+userSchema.methods.send_emailWelcome = async function (cb) {
+    const tokenVerify = new Token({
+        _userId: this.id,
+        token: crypto.randomBytes(16).toString("hex")
+    })
+    const email_destination = this.email
+    tokenVerify.save((err) => {
+        if (err) {
+            return console.log(err.message)
+        }
 
         const emailOptions = {
-            from: 'FooodFAST',
+            from: "FooodFAST",
             to: email_destination,
             subject: "check e-mail",
-            html: `<a href= "http://localhost:3001/api/v1/auth/tokenConfirmed/${tokenVerify.token}">verifique su cuenta aqui</a>` 
-        };
-        emailer.sendMail(emailOptions, (err)=>{
-            if(err){return console.log(err.message)};
-            console.log("A verification email has been sent to ", email_destination)
+            html: `<a href= "${process.env.BACK_URL}/api/v1/auth/tokenConfirmed/${tokenVerify.token}">verifique su cuenta aqui</a>`
+        }
+        emailer.sendMail(emailOptions, (err) => {
+            if (err) {
+                return console.log(err.message)
+            }
+            console.log(
+                "A verification email has been sent to ",
+                email_destination
+            )
         })
-    }) 
+    })
 }
-const User = mongoose.model("User",userSchema)
-export default User;
+const User = mongoose.model("User", userSchema)
+export default User
